50b256d74ea1bb0e91b9f2b01440eec4215f6266
Maxime Bret <maxime.bret.ext@boursorama.fr>
2015-09-14 17:51:49 +0200

    add new path to handle scripts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var path    = require('path');
 
 // Define constants
-process.env.APP_ROOT_PATH   = path.join(__dirname, '..', '..');
+process.env.APP_ROOT_PATH   = path.join(__dirname, '../..');
 process.env.SCRIPT_PATH     = path.join(__dirname, 'lib/script.js');
 process.env.SCRIPTS_PATH    = path.join(__dirname, 'scripts');
 process.env.LIB_PATH        = path.join(__dirname, 'lib');
